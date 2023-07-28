d95aa7782a4198be5623c19a9ce8890e88498102
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-07-28 20:55:05 +0600

    fixed navbar for mobile menu

--- src/components/Layouts/Sidebar/Sidebar.jsx
+++ src/components/Layouts/Sidebar/Sidebar.jsx
@@ -1,15 +1,20 @@
 import Hamburger from 'hamburger-react'
 import Link from 'next/link'
 import React from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
 
 const Sidebar = ({ isOpen, setOpen }) => {
   return (
-    <div
-      className={`absolute top-0 w-1/2 bg-gray-500 min-h-screen ${
-        isOpen
-          ? 'left-0 transition-left duration-700'
-          : '-left-1/2 transition-left duration-700'
-      } `}
+    <motion.div
+      animate={{
+        width: isOpen ? '300px' : '0px',
+        transition: {
+          duration: 0.5,
+          type: 'spring',
+          damping: 20,
+        },
+      }}
+      className={`fixed w-[300px] top-0 left-0 z-50 bg-gray-500 h-screen overflow-x-hidden`}
     >
       <header className='flex justify-between items-center'>
         <h2 className='text-xl font-bold'>PC Builder</h2>
@@ -20,7 +25,7 @@ const Sidebar = ({ isOpen, setOpen }) => {
           <Link href='/'>Home </Link>
         </li>
       </ul>
-    </div>
+    </motion.div>
   )
 }
 
