ab05e6dd28007a1f3b373675796305ae75cbe827
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-07-29 19:49:48 +0600

    complete all category product data

--- src/pages/categories/others.jsx
+++ src/pages/categories/others.jsx
@@ -1,11 +1,49 @@
 import PcComponentLayout from '@/components/Layouts/PcComponentLayout/PcComponentsLayout'
 import RootLayout from '@/components/Layouts/RootLayout'
 import React from 'react'
+import { Card } from 'antd'
+import Link from 'next/link'
+import Image from 'next/image'
+const { Meta } = Card
 
-const others = () => {
+const others = ({ filterData }) => {
   return (
-    <div>
-      <h2>Others</h2>
+    <div className='w-full h-full'>
+      {filterData?.data?.length === 0 ? (
+        <div className='w-full h-full text-xl font-bold flex justify-center items-center'>
+          No Data Matched
+        </div>
+      ) : (
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2'>
+          {filterData?.data?.map((data, index) => (
+            <div
+              key={index}
+            >
+              <Link href={`/${data?._id}`}>
+                <Card
+                  className='bg-[#f2f4f8]'
+                  hoverable
+                  cover={
+                    <Image
+                      className='p-4'
+                      width={100}
+                      height={100}
+                      layout='responsive'
+                      alt='example'
+                      src='/pc.jpg'
+                    />
+                  }
+                >
+                  <Meta title={data?.name} />
+                  <p className='my-2 text-gray-500 font-bold'>
+                    Price: $<span>{data?.price}</span>
+                  </p>
+                </Card>
+              </Link>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
@@ -18,4 +56,17 @@ others.getLayout = function getLayout(page) {
       <PcComponentLayout>{page}</PcComponentLayout>
     </RootLayout>
   )
+}
+
+export const getStaticProps = async () => {
+  const response = await fetch(
+    'http://localhost:5000/api/v1/products?category=others'
+  )
+  const filterData = await response.json()
+
+  return {
+    props: {
+      filterData: filterData,
+    },
+  }
 }
